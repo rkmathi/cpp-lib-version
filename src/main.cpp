7e6ef792ced351c335e504c561c4f6f7cf354130
Ryuichi KAWAMATA <iam@rkmathi.net>
2017-04-07 11:25:04 +0900

    Fix comment

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,19 @@ int main(void) {
   auto cpp14 = [](auto x, auto y) {return x + y;};
   std::cout << cpp14(11, 3) << "\n";
 
-
+  // c++11 library
   std::cout << "fuga::Fuga::version():" << fuga::Fuga::version() << "\n";
   fuga::Fuga f;
   std::cout << "f.i:" << f.i() << ":"
             << "f.d:" << f.d() << "\n";
 
+  // c++98 library
   std::cout << "hoge::Hoge::version():" << hoge::Hoge::version() << "\n";
   hoge::Hoge h;
   std::cout << "h.i:" << h.i() << ":"
             << "h.d:" << h.d() << "\n";
 
+  // c99 library
   std::cout << "piyo_version():" << piyo_version() << "\n";
   piyo_Piyo p;
   piyo_InitPiyo(&p);
